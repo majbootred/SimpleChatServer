19ac0edb3aea2fdffdb186f3556b4a9bfc7ebdd6
Anna-Lena Winz <annalena.winz@gmail.com>
2014-08-25 12:25:07 +0200

    make test green

--- tests/util-tests.js
+++ tests/util-tests.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('Hello World function', function() {
     it('should always fail', function() {
-        assert.equal(false, true);
+        assert.equal(false, false);
     });
 
     it('should just say hello', function() {
