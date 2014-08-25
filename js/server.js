e82eacbf55f064e91136fa90e20c533aacdd4f96
Marlen Jacob <majbootred@gmail.com>
2014-08-25 16:24:46 +0200

    refactored version

--- js/server.js
+++ js/server.js
@@ -1,14 +1,28 @@
+const port = 1337;
 var http = require('http');
 var util = require('./util');
-var count = 0;
+var userCount = 0;
 
-http.createServer(function (req, res) {
-    count = count + 1;
-    console.log("User:"+count);
+function increaseUserCountByOne(){
+    userCount++;
+    console.log("User:"+userCount);
+}
+
+function sendResponseWithHelloWorld(response){
     var answer =  util.helloWorld();
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(answer);
-}).listen(1337, '127.0.0.1');
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(answer);
+}
 
-console.log('Server running at http://127.0.0.1:1337/');
+var handleDefaultRequest = function (req, res) {
+    increaseUserCountByOne();
+    sendResponseWithHelloWorld(res);
+};
 
+var server = http.createServer();
+
+server.on('request',handleDefaultRequest);
+
+server.listen(port, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:'+port+'/');
