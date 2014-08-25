79f05fdc05d0f6e1a070e642938121bb0903b1bb
bilgegunduz <bilge@eduroam-pool7-2009.wlan.uni-bremen.de>
2014-08-25 15:25:53 +0200

    first commit

--- js/server.js
+++ js/server.js
@@ -1,11 +1,14 @@
 var http = require('http');
-var util = require('./util');
+//var util = require('./util');
+var userInfo = require('./userInfo');
+var counter= 0;
 
 http.createServer(function (req, res) {
-    var answer =  util.helloWorld();
+    //var answer =  util.helloWorld();
+    var newuser = userInfo.tellNewUser();
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(answer);
-}).listen(1337, '127.0.0.1');
+    //res.end(answer);
+}).listen(1337, 'bilge.local');
 
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://bilge.local:1337/');
 
