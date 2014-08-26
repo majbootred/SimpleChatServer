9c21db4bdc72dafce7ccb19ef46656562a9fbb50
Dr Blinken <drblinken@gmail.com>
2014-08-26 16:32:55 +0200

    working on array with active chat clients - not quite working

--- js/chat-server.js
+++ js/chat-server.js
@@ -2,7 +2,7 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
-
+var chatClients = new Array();
 app.listen(4711);
 
 function handler (req, res) {
@@ -18,11 +18,28 @@ function handler (req, res) {
         });
 }
 
+function deleteFromArray(chatClients,socket){
+    var newArray = [];
+    for (var i=0;i<chatClients.length;i++){
+        if (chatClients[i] === socket){
+
+        } else {
+            newArray.push(chatClients[i]);
+        }
+    }
+    chatClients = newArray;
+    return chatClients.length;
+}
 io.on('connection', function (socket) {
+    var numberOfClients = chatClients.push(socket);
+    console.log("new client connected, clients: "+numberOfClients);
+
     socket.emit('news', { hello: 'world' });
     socket.on('my other event', function (data) {
         console.log(data);
     socket.on('disconnect', function() {
+            var numberOfClients = deleteFromArray(chatClients,socket);
+            console.log("client disconnected; remaining clients: "+numberOfClients);
             console.log("Bye, bye");
         });
     });
