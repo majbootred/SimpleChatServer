46768b945d87350cfe326cdded01b15039f15402
Marlen Jacob <majbootred@gmail.com>
2014-08-26 11:20:08 +0200

    url pathname extract with JSON parse

--- js/server.js
+++ js/server.js
@@ -1,15 +1,25 @@
-const port = 1337;
+const Port = 1337;
 var http = require('http');
 var util = require('./util');
 var userCount = 0;
+var url = require('url');
+var urlo;
 
-function increaseUserCountByOne(){
-    userCount++;
-    console.log("User:"+userCount);
+function getURLfromBrowser(req){
+    urlo = url.parse(req.url);
+   console.log(urlo);
+    console.log('////////');
+   console.log(urlo['pathname'].split('/')[1]) ;
+
 }
 
-function sendResponseWithHelloWorld(response){
-    var answer =  util.helloWorld();
+function increaseUserCountByOne() {
+    userCount++;
+    //console.log("User:" + userCount);
+}
+
+function sendResponseWithHelloWorld(response) {
+    var answer = util.helloWorld();
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end(answer);
 }
@@ -17,12 +27,13 @@ function sendResponseWithHelloWorld(response){
 var handleDefaultRequest = function (req, res) {
     increaseUserCountByOne();
     sendResponseWithHelloWorld(res);
+    getURLfromBrowser(req);
 };
 
 var server = http.createServer();
 
-server.on('request',handleDefaultRequest);
+server.on('request', handleDefaultRequest);
 
-server.listen(port, '127.0.0.1');
+server.listen(Port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:'+port+'/');
+console.log('Server running at http://127.0.0.1:' + Port + '/');
