8801242caf8e72f3b201647898faf7790e822a28
Marlen Jacob <majbootred@gmail.com>
2014-08-26 12:18:45 +0200

    with comments

--- js/server.js
+++ js/server.js
@@ -3,25 +3,23 @@ var http = require('http');
 var util = require('./util');
 var userCount = 0;
 var url = require('url');
-
-function getURLfromBrowser(req){
+/*fuction parse the url in to a string and call getPathnameFromURL*/
+function getURLfromBrowser(req) {
     var str = url.format(req.url);
     getPathnamefromURL(str);
 }
-
+/*function extracts the username from url an call the function to print the username*/
 function getPathnamefromURL(str) {
     var username = str.substr(1, str.length);
     printUsernameFromPathname(username);
 }
-
+/*function prints the username in the console*/
 function printUsernameFromPathname(username) {
     if (username != 'favicon.ico') {
         console.log("Hello, " + username);
     }
 
 }
-
-
 
 function increaseUserCountByOne() {
     userCount++;
