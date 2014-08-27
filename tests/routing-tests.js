72287741b99d282a3ec31181a3a583397ead2e01
julialpz <julialpz@gmx.de>
2014-08-27 11:23:09 +0200

    a routing test

--- tests/routing-tests.js
+++ tests/routing-tests.js
@@ -1,9 +1,20 @@
 var util = require('../js/util');
+var server = require('../js/server')
 var assert = require('assert');
 
 describe('Routing test', function() {
     it('should remove the first char from string', function() {
-        assert.equal("emma", util.getPathnamefromURL("/emma"));
+        assert.equal("emma", util.deleteSlashFromPathName("/emma"));
+    });
+
+    it('should print hello emma to emma', function() {
+        var str = "emma";
+        assert.equal("Hello emma!", server.printHelloUser(str));
+    });
+
+    it('should ??? when requesting favicon', function() {
+        var str = "favicon.ico";
+        assert.equal(null, server.printHelloUser(str));
     });
 });
 
