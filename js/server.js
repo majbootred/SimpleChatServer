2794b16349e0a366aff6e7233798992093786112
Marlen Jacob <majbootred@gmail.com>
2014-08-28 09:48:13 +0200

    server.js added again

--- js/server.js
+++ js/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app=express();
 
 app.get('/', function(req,res){
-    res.send('<a href ="http://localhost:1337/myname">http://localhost:1337/myname</a> ')
+    res.send('<a href ="http://localhost:1337/myname">http://localhost:1337/myname</a> ');
 
 });
 app.get('/statistics', function(req,res){
