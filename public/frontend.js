d4cacdf7385cde289b5cd00a088a4f29c90ec163
Marlen Jacob <majbootred@gmail.com>
2014-08-29 11:57:23 +0200

    nicer styles

--- public/frontend.js
+++ public/frontend.js
@@ -1,20 +1,23 @@
-var socket = io();
-var newUser;
+$(document).ready(function() {
+    var socket = io();
+    var newUser;
 
-$('form').submit(function(){
-    socket.emit('user name', $('#user_name').val());
-    socket.emit('chat message', $('#input_message').val());
-    $('#input_message').val('');
-    console.log('user name: ' + $('#user_name').val());
-    return false;
+    $('form').submit(function(){
+        socket.emit('user name', $('#user_name').val());
+        socket.emit('chat message', $('#input_message').val());
+        $('#input_message').val('');
+        console.log('user name: ' + $('#user_name').val());
+        return false;
+    });
+
+    socket.on('broadcast', function (msg) {
+        var prevContent = $('#messages').val();
+        var newContent = prevContent + msg + "\n";
+        $('#messages').val(newContent);
+        var textArea = $('#messages');
+        textArea.scrollTop(textArea[0].scrollHeight - textArea.height());
+
+
+    });
 });
 
-socket.on('broadcast', function (msg) {
-    var prevContent = $('#messages').val();
-    var newContent = prevContent + msg + "\n";
-    $('#messages').val(newContent);
-    var textArea = $('#messages');
-    textArea.scrollTop(textArea[0].scrollHeight - textArea.height());
-
-
-});
